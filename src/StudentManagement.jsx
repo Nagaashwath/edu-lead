92b8bb135c0698b7be0ae0cf388b39438e2d42f3
Nagaashwath M N <Nagaashwath_M_n@Dell.com>
2021-07-31 01:26:52 +0530

    adding student management component

---
+++ src/StudentManagement.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Pagination from './common/pagination';
+
+const StudentManagement=()=>{
+return <div>
+
+{'Student'}
+    <Pagination />
+</div>
+}
+
+export default StudentManagement;
---
+++ src/common/pagination.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Pagination =(props)=>{
+    return (<React.Fragment>
+
+        {'Pagination'}
+    </React.Fragment>);
+}
+
+export default Pagination;
